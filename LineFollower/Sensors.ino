89e9d7a91ba6ada58be61b3394dc9414c5b5ed2f
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-15 07:01:56 +0300

    Create Sensors.ino

---
+++ LineFollower/Sensors.ino
@@ -0,0 +1,29 @@
+void setupSensors() {
+  for (int i = 0; i < 12; i++) {
+    pinMode(sensorPins[i], INPUT);
+  }
+}
+
+void calculateError() {
+  long weightedSum = 0;
+  long sum = 0;
+  int rawValues[12];
+  
+  for (int i = 0; i < 12; i++) {
+    rawValues[i] = analogRead(sensorPins[i]);
+    
+    // فلترة اللون الأسود
+    if (rawValues[i] > 2000) {
+      weightedSum += (long)rawValues[i] * sensorWeights[i];
+      sum += rawValues[i];
+    }
+  }
+
+  if (sum > 0) {
+    currentError = (float)weightedSum / (float)sum;
+    lastError = currentError;
+  } else {
+    // ذاكرة الروبوت إذا فقد الخط
+    currentError = (lastError > 0) ? 60 : -60; 
+  }
+}
