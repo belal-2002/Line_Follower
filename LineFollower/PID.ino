e48ba83167bf5d16cc7cfe625fb1de416b47fe7e
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-16 07:41:34 +0300

    Update PID.ino

--- LineFollower/PID.ino
+++ LineFollower/PID.ino
@@ -8,10 +8,10 @@ void calculatePID() {
   leftMotorSpeed  = baseSpeed + PID_Value;
   rightMotorSpeed = baseSpeed - PID_Value;
 
-  // تقييد السرعة حتى لا تتجاوز 255 أو تنزل عن الصفر
-  if (leftMotorSpeed > 255) leftMotorSpeed = 255;
+  // تقييد السرعة حتى لا تتجاوز 1023 أو تنزل عن الصفر
+  if (leftMotorSpeed > 1023) leftMotorSpeed = 1023;
   if (leftMotorSpeed < 0) leftMotorSpeed = 0;
-  if (rightMotorSpeed > 255) rightMotorSpeed = 255;
+  if (rightMotorSpeed > 1023) rightMotorSpeed = 1023;
   if (rightMotorSpeed < 0) rightMotorSpeed = 0;
 
   // إرسال السرعة المحسوبة للمحركات
