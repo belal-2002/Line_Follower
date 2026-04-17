4ee821ac88597473af8f1887de9a121779f59e3c
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-17 17:16:58 +0300

    Update Sensors.ino

--- LineFollower/Sensors.ino
+++ LineFollower/Sensors.ino
@@ -3,7 +3,7 @@ void loopSensors() {
   for (int i = 0; i < 12; i++) {
     sensorValue[i] = analogRead(sensorPins[i]);
   }
-  sensorValue[1] -= 270;
+  //sensorValue[1] -= 270;
   sensorValue[10] -= 270;
 }
 
