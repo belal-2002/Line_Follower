0affb4baf2bebf7356e2c2616e97d79d6e3c46be
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-17 17:05:43 +0300

    Update Motors.ino

--- LineFollower/Motors.ino
+++ LineFollower/Motors.ino
@@ -1,64 +1,33 @@
-
-
+void loopMotor() {
+  if (lineAvailable) {
   // إرجاع المحركات للاتجاه الأمامي الطبيعي
   digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
   digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW);
-
   // تقييد السرعة وإرسالها للمحركات (10-bit)
   int leftMotorSpeed  = constrain(baseSpeed + PD_Value, 0, 1023);
   int rightMotorSpeed = constrain(baseSpeed - PD_Value, 0, 1023);
 
   ledcWrite(PWMA, leftMotorSpeed);
   ledcWrite(PWMB, rightMotorSpeed);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // ==========================================
-  // المقاطعة العنيفة: التفاف 90 درجة (Tank Turn)
-  // ==========================================
-  if (currentState == SHARP_LEFT) {
-    // عكس المحرك الأيسر للخلف، ودفع الأيمن للأمام
-    digitalWrite(AIN1, HIGH); digitalWrite(AIN2, LOW); 
-    digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW); 
+  } else {
+    if (goRight) {
+      // دفع المحرك الأيسر للأمام، وعكس الأيمن للخلف
+      digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
+      digitalWrite(BIN1, LOW); digitalWrite(BIN2, HIGH); 
     
-    ledcWrite(PWMA, turnSpeed);
-    ledcWrite(PWMB, turnSpeed);
+      ledcWrite(PWMA, turnSpeed);
+      ledcWrite(PWMB, turnSpeed);
     
-    lastError = -40; // زرع ذاكرة وهمية في حال فقدان الخط لضمان استمرار الدوران
-    delay(250);
-    return; // الخروج من الدالة (إعدام الـ PD مؤقتاً)
-  } 
-  else if (currentState == SHARP_RIGHT) {
-    // دفع المحرك الأيسر للأمام، وعكس الأيمن للخلف
-    digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
-    digitalWrite(BIN1, LOW); digitalWrite(BIN2, HIGH); 
+      lastError = 40; 
+    } else {
+      // عكس المحرك الأيسر للخلف، ودفع الأيمن للأمام
+      digitalWrite(AIN1, HIGH); digitalWrite(AIN2, LOW); 
+      digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW); 
     
-    ledcWrite(PWMA, turnSpeed);
-    ledcWrite(PWMB, turnSpeed);
+      ledcWrite(PWMA, turnSpeed);
+      ledcWrite(PWMB, turnSpeed);
     
-    lastError = 40; 
-
-    delay(250);
-    return; // الخروج من الدالة
+      lastError = -40;
+    }
   }
+}
