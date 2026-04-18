d351ed923cdc09e616cf53fbb22b72bc61ec49c1
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-18 18:37:37 +0300

    Update Print.ino

--- LineFollower/Print.ino
+++ LineFollower/Print.ino
@@ -19,7 +19,7 @@ void loopPrint() {
       }
 
       TelnetStream.print("\t");
-      TelnetStream.printf("Kp:%.1f  Kd:%.1f  maximumSpeed:%d  Speed:%d  TSpeed:%d", Kp, Kd, maximumSpeed, baseSpeed, turnSpeed);
+      TelnetStream.printf("Kp:%.2f  Kd:%.1f  maxSpeed:%d  Speed:%d  TSpeed:%d  Err:%d", Kp, Kd, maximumSpeed, baseSpeed, turnSpeed, PD_Value);
       TelnetStream.print("\t");
       //TelnetStream.printf("Err:%4.1f | ML:%d MR:%d", currentError, leftMotorSpeed, rightMotorSpeed);
 
@@ -31,16 +31,16 @@ void loopPrint() {
   // معالجة أوامر الكيبورد لتعديل PID
   if (TelnetStream.available()) {
     char c = TelnetStream.read();
-    if (c == 'q') Kp += 1;
-    if (c == 'a') Kp -= 1;
+    if (c == 'q') Kp += 0.1;
+    if (c == 'a') Kp -= 0.1;
     if (c == 'w') Kd += 1;
     if (c == 's') Kd -= 1;
-    if (c == 'e') maximumSpeed += 50;
-    if (c == 'd') maximumSpeed -= 50;
-    if (c == 'r') baseSpeed += 50;
-    if (c == 'f') baseSpeed -= 50;
-    if (c == 't') turnSpeed += 50;
-    if (c == 'g') turnSpeed -= 50;
+    if (c == 'e') maximumSpeed += 25;
+    if (c == 'd') maximumSpeed -= 25;
+    if (c == 'r') baseSpeed += 25;
+    if (c == 'f') baseSpeed -= 25;
+    if (c == 't') turnSpeed += 25;
+    if (c == 'g') turnSpeed -= 25;
 
     if (Kp < 0) Kp = 0;
     if (Kd < 0) Kd = 0;
