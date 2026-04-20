564fd7dde27a80db1a17fe89695bd043ca64dfd9
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-20 18:14:27 +0300

    Update Motors.ino

--- LineFollower/Motors.ino
+++ LineFollower/Motors.ino
@@ -1,29 +1,54 @@
 void loopMotor() {
-  if (lineAvailable) {
-  // إرجاع المحركات للاتجاه الأمامي الطبيعي
-  digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
-  digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW);
-  // تقييد السرعة وإرسالها للمحركات (10-bit)
-  leftMotorSpeed  = constrain(baseSpeed + PD_Value, 0, maximumSpeed);
-  rightMotorSpeed = constrain(baseSpeed - PD_Value, 0, maximumSpeed);
+  switch (caseMotor) {
+    
+    case 0:
+      // إرجاع المحركات للاتجاه الأمامي الطبيعي
+      digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
+      digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW);
+      // تقييد السرعة وإرسالها للمحركات (10-bit)
+      leftMotorSpeed  = constrain(baseSpeed + PD_Value, 0, maximumSpeed);
+      rightMotorSpeed = constrain(baseSpeed - PD_Value, 0, maximumSpeed);
 
-  ledcWrite(PWMA, leftMotorSpeed);
-  ledcWrite(PWMB, rightMotorSpeed);
-  } else {
-    if (goRight) {
-      // دفع المحرك الأيسر للأمام، وعكس الأيمن للخلف
+      ledcWrite(PWMA, leftMotorSpeed);
+      ledcWrite(PWMB, rightMotorSpeed);
+    break;
+
+    case 1:
+      // الدوران لليمين
       digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
       digitalWrite(BIN1, LOW); digitalWrite(BIN2, HIGH); 
     
       ledcWrite(PWMA, turnSpeed);
       ledcWrite(PWMB, turnSpeed);
-    } else {
-      // عكس المحرك الأيسر للخلف، ودفع الأيمن للأمام
+    break;
+
+    case 2:
+      // الدوران لليسار
       digitalWrite(AIN1, HIGH); digitalWrite(AIN2, LOW); 
       digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW); 
     
       ledcWrite(PWMA, turnSpeed);
       ledcWrite(PWMB, turnSpeed);
-    }
+    break;
+
+    case 3:
+
+    break;
+
+    case 4:
+
+    break;
+
+    case 5:
+
+    break;
+
+    case 6:
+
+    break;
+
+    case 7:
+
+    break;
   }
 }
