4d0a2b7c0397f9abc332c669a4f037a61e0fcb43
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-20 17:39:39 +0300

    Update Strategy.ino

--- LineFollower/Strategy.ino
+++ LineFollower/Strategy.ino
@@ -1,3 +1,36 @@
 void loopStrategy() {
+  switch (strategy) {
+    
+    case 0: // الحالة 000
 
+      break;
+
+    case 1: // الحالة 001
+
+      break;
+
+    case 2: // الحالة 010
+      // ضع قيمك هنا
+      break;
+
+    case 3: // الحالة 011
+      // ضع قيمك هنا
+      break;
+
+    case 4: // الحالة 100
+      // ضع قيمك هنا
+      break;
+
+    case 5: // الحالة 101
+      // ضع قيمك هنا
+      break;
+
+    case 6: // الحالة 110
+      // ضع قيمك هنا
+      break;
+
+    case 7: // الحالة 111
+
+      break;
+  }
 }
