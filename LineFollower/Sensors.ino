f0b16cd063c3cf4759cf8952a693139b23f754cf
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-20 18:32:51 +0300

    Update Sensors.ino

--- LineFollower/Sensors.ino
+++ LineFollower/Sensors.ino
@@ -5,5 +5,15 @@ void loopSensors() {
   }
   sensorValue[1] -= 70;
   sensorValue[10] -= 170;
+   
+  for (int i = 0; i < 12; i++) {
+    if (sensorValue[i] > lineThreshold) {
+      TelnetStream.print(1);  
+      TelnetStream.print("\t");
+    } else {
+      TelnetStream.print(0);  
+      TelnetStream.print("\t");
+    }
+  }
 }
 
