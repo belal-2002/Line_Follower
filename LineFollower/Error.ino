41c5ac850a80aa3e175f50aeeebbf50c21e278c2
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-20 20:28:45 +0300

    Update Error.ino

--- LineFollower/Error.ino
+++ LineFollower/Error.ino
@@ -1,9 +1,25 @@
 void calculateError() {
+  rightSensor = 0;
+  leftSensor = 0;
+  allSensor = 0;
   weightedSum = 0;
   sum = 0;
 
+  for (int i = 0; i < 6; i++) {
+    if (bitRead(sensorBit, i)) rightSensor ++;
+  }
+  for (int i = 6; i < 12; i++) {
+    if (bitRead(sensorBit, i)) leftSensor ++;
+  }  
+  allSensor = rightSensor + leftSensor;
+
+  if (allSensor > 9) {  // السير في خط مسطقيم
+    caseMotor = 3;
+    return;
+  }
+
   for (int i = 0; i < 12; i++) {
-    if (sensorValue[i] > lineThreshold) {
+    if (bitRead(sensorBit, 11 - i)) {
       weightedSum += (long)sensorValue[i] * sensorWeights[i];
       sum += sensorValue[i];
     }
@@ -27,19 +43,15 @@ void calculateError() {
   
   } else {
     lineAvailable = false; // الروبوت فقد الخط كلياً في المسار الطبيعي
-    if (goRight){
-      // الدوران لليمين
+    if (goRight){ // الدوران لليمين
       caseMotor = 1;
-    } else {
-      // الدوران لليسار
+    } else { // الدوران لليسار
       caseMotor = 2;
     }
   }
   lineWasFound = lineAvailable;
+
+  if (lineAvailable) calculatePD();
 }
-
-
-
-
 
 
