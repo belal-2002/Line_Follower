a97338d7dc25e6719b532bf2f4be0a6bd56887b7
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-22 02:04:40 +0300

    Update Sensors.ino

--- LineFollower/Sensors.ino
+++ LineFollower/Sensors.ino
@@ -13,5 +13,19 @@ void loopSensors() {
       bitClear(sensorBit, 11 - i);
     }
   }
+
+  // استخراج أول 6 بتات (من 0 إلى 5) الخاصة بالحساسات اليمنى وعدّها بسرعة
+  rightSensor = __builtin_popcount(sensorBit & 0x3F); // 0x3F تعادل 000000111111 ثنائياً
+
+  // إزاحة البتات 6 خطوات لليمين لاستخراج بتات الحساسات اليسرى (من 6 إلى 11) وعدّها
+  leftSensor = __builtin_popcount((sensorBit >> 6) & 0x3F); 
+  midSensor = __builtin_popcount((sensorBit >> 2) & 0xFF);
+
+  // إجمالي الحساسات
+  allSensor = __builtin_popcount(sensorBit & 0xFFF);
+
+  leftRadar = bitRead(sensorBit, 0) + bitRead(sensorBit, 1);
+  rightRadar = bitRead(sensorBit, 10) + bitRead(sensorBit, 11);
+  radar = leftRadar + rightRadar;  
 }
 
