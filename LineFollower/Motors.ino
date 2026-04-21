ffd1a792b169b67797ff52fde1a7c866d608d688
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-22 02:04:45 +0300

    Update Motors.ino

--- LineFollower/Motors.ino
+++ LineFollower/Motors.ino
@@ -1,6 +1,5 @@
 void loopMotor() {
   switch (caseMotor) {
-    
     case 0:
       // إرجاع المحركات للاتجاه الأمامي الطبيعي
       digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
@@ -12,7 +11,6 @@ void loopMotor() {
       ledcWrite(PWMA, leftMotorSpeed);
       ledcWrite(PWMB, rightMotorSpeed);
     break;
-
     case 1:
       // الدوران لليمين
       digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
@@ -21,7 +19,6 @@ void loopMotor() {
       ledcWrite(PWMA, turnSpeed);
       ledcWrite(PWMB, turnSpeed);
     break;
-
     case 2:
       // الدوران لليسار
       digitalWrite(AIN1, HIGH); digitalWrite(AIN2, LOW); 
@@ -30,7 +27,6 @@ void loopMotor() {
       ledcWrite(PWMA, turnSpeed);
       ledcWrite(PWMB, turnSpeed);
     break;
-
     case 3:
       // السير في خط مستقيم
       digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
@@ -39,19 +35,15 @@ void loopMotor() {
       ledcWrite(PWMA, turnSpeed);
       ledcWrite(PWMB, turnSpeed);
     break;
-
     case 4:
 
     break;
-
     case 5:
 
     break;
-
     case 6:
 
     break;
-
     case 7:
 
     break;
