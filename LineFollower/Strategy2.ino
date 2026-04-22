fbe79f7cdd5461f0e7401af37ab7368e5da27381
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-22 18:47:10 +0300

    Create Strategy2.ino

---
+++ LineFollower/Strategy2.ino
@@ -0,0 +1,15 @@
+/*
+      // بس يخلص الخط الاسود لف
+      if (midSensor >= 7) {  // السير في خط مستقيم لتجاوز التقاطع
+        caseMotor = 3;
+        lineWasFound = false;
+        lineAvailable = false;
+        loopMotor();
+        return;
+      }
+
+      // لازم افحص بت معين
+      if (leftRadar == 2) {
+    
+      }
+      */  
