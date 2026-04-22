1a3eaee96036e28b0f749e87b6808a156c3e4171
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-22 18:47:17 +0300

    Update Strategy.ino

--- LineFollower/Strategy.ino
+++ LineFollower/Strategy.ino
@@ -1,62 +1,28 @@
 void loopStrategy() {
   switch (strategy) {
     case 0: // 000
-      if (midSensor >= 7) {  // السير في خط مستقيم لتجاوز التقاطع
-        caseMotor = 3;
-        lineWasFound = false;
-        lineAvailable = false;
-        loopMotor();
-        return;
-      }
-
-      // لازم افحص بت معين
-      if (leftRadar == 2) {
-    
-      }
+      loopStrategy0();
     break;
     case 1: // 001
-      // بس يخلص الخط الاسود لف
-      if (midSensor >= 7) {  // السير في خط مستقيم لتجاوز التقاطع
-        caseMotor = 3;
-        lineWasFound = false;
-        lineAvailable = false;
-        loopMotor();
-        return;
-      }
-
-      // لازم افحص بت معين
-      if (leftRadar == 2) {
-    
-      }
+      loopStrategy1();  
     break;
     case 2: // 010
-      if (allSensor >= 11 || radar >= 3) {  // السير في خط مستقيم لتجاوز التقاطع
-        caseMotor = 3;
-        lineWasFound = false;
-        lineAvailable = false;
-        loopMotor();
-        return;
-      }
-
-      // لازم افحص بت معين
-      if (leftRadar == 2) {
-    
-      }
+      loopStrategy2();
     break;
     case 3: // 011
-
+      loopStrategy3();
     break;
     case 4: // 100
-
+      loopStrategy4();
     break;
     case 5: // 101
-
+      loopStrategy5();
     break;
     case 6: // 110
-
+      loopStrategy6();
     break;
     case 7: // 111
-
+      loopStrategy7();
     break;
   }
 }
