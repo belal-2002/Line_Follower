57cf8ab66d14c51ec7890997ffc061383b704a56
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-22 18:47:06 +0300

    Create Strategy3.ino

---
+++ LineFollower/Strategy3.ino
@@ -0,0 +1,14 @@
+/*
+      if (allSensor >= 11 || radar >= 3) {  // السير في خط مستقيم لتجاوز التقاطع
+        caseMotor = 3;
+        lineWasFound = false;
+        lineAvailable = false;
+        loopMotor();
+        return;
+      }
+
+      // لازم افحص بت معين
+      if (leftRadar == 2) {
+    
+      }
+*/      
