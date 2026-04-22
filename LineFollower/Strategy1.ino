4f9a36e63fee0b711cf4df905082f1b520a112eb
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-22 18:47:12 +0300

    Create Strategy1.ino

---
+++ LineFollower/Strategy1.ino
@@ -0,0 +1,14 @@
+/*
+      if (leftSensor)
+      if (leftRadar)
+
+      if (midSensor >= 7) {
+        calculateError();
+        return;
+      }
+
+      // لازم افحص بت معين
+      if (leftRadar == 2) {
+    
+      }
+      */  
