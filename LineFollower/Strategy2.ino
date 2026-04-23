120aad5357e810c201ec15f69129f19478bf41a0
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-23 16:03:08 +0300

    Update Strategy2.ino

--- LineFollower/Strategy2.ino
+++ LineFollower/Strategy2.ino
@@ -1,33 +1,35 @@
-void loopStrategy2() { 
+void loopStrategy2() { //Left
   if ((bitRead(sensorBit, 11)) && (midSensor >= 2) && (!rightRadar)) {
     lineWasFound = false;
     caseMotor = 1;
     goLeft = true; 
     loopMotor();
-    delay(250);
+    delay(127);
     return;   
   }
 
-  if (midSensor && (!leftRadar)) { 
+  if ((bitRead(sensorBit, 5)) || (bitRead(sensorBit, 6)) && (!leftRadar)) {
     goLeft = false;
     goRight = false;
   }
-  
-  if (goLeft && leftRadar) { goLeft = false; calculateError(); return; }
-  if (goRight && rightRadar) { goRight = false; calculateError(); return; }
-  
+
+  //if (goLeft && leftRadar) { goLeft = false; calculateError(); return; }
+  //if (goRight && rightRadar) { goRight = false; calculateError(); return; }
+
   if (goLeft || goRight) return;
 
   if (!allSensor) {
     lineWasFound = false;
     if (leftRadarOn) {
       caseMotor = 1;
-      goLeft = true; 
+      goLeft = true;
+      turnStartTime = millis(); 
       return;
     }
     if (rightRadarOn) {
       caseMotor = 2;
       goRight = true;
+      turnStartTime = millis();
       return;
     }
   }
