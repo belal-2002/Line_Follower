2770e16a90848ea9081af94bed45d4cfe7bd9d12
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-23 11:05:21 +0300

    Update Strategy1.ino

--- LineFollower/Strategy1.ino
+++ LineFollower/Strategy1.ino
@@ -1,14 +1,39 @@
-/*
-      if (leftSensor)
-      if (leftRadar)
+void loopStrategy1() { 
+  // إلغاء الدوران الأعمى فور ملامسة حساسات المنتصف للخط
+  if (midSensor) { 
+    goLeft = false;
+    goRight = false;
+  }
+  
+  // الاستشفاء المبكر (إنهاء الدوران فور التقاط الرادار للخط)
+  if (goRight && rightRadar) { goRight = false; calculateError(); return; }
+  if (goLeft && leftRadar) { goLeft = false; calculateError(); return; }
+  
+  // الاستمرار في الدوران إذا بدأناه
+  if (goLeft || goRight) return;
 
-      if (midSensor >= 7) {
-        calculateError();
-        return;
-      }
+  // الدخول في حالة الفقدان الكلي للخط
+  if (!allSensor) {
+    lineWasFound = false;
+    if (rightRadarOn) {
+      caseMotor = 2;
+      goRight = true;
+      return;
+    }
+    if (leftRadarOn) {
+      caseMotor = 1;
+      goLeft = true; 
+      return;
+    }
+  }
 
-      // لازم افحص بت معين
-      if (leftRadar == 2) {
-    
-      }
-      */  
+  // تجاوز التقاطعات العرضية بأمان
+  if (radar && midSensor){
+    bitClear(sensorBit, 0);
+    bitClear(sensorBit, 1);
+    bitClear(sensorBit, 10);
+    bitClear(sensorBit, 11);
+  }
+  
+  calculateError();
+}      
