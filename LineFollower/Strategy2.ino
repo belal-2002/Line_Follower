4092cf2c2dfd8ab096c27d07ed2602c651ffb69b
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-23 12:25:13 +0300

    Update Strategy2.ino

--- LineFollower/Strategy2.ino
+++ LineFollower/Strategy2.ino
@@ -30,7 +30,16 @@ void loopStrategy2() {
     }
   }
 
-  if (rightRadar && midSensor){
+  // 1. اكتشاف تقاطع الزائد (+) وتجاوزه مستقيماً
+  if (leftRadar && rightRadar && midSensor) {
+    // إعماء الرادارين معاً ليمر الروبوت بناءً على حساسات المنتصف فقط
+    bitClear(sensorBit, 0);
+    bitClear(sensorBit, 1);
+    bitClear(sensorBit, 10);
+    bitClear(sensorBit, 11);
+  }
+  // 2. تجاهل الفخاخ اليمنى (حرف T المتجه لليمين)
+  else if (rightRadar && midSensor) {
     bitClear(sensorBit, 0);
     bitClear(sensorBit, 1);
   }
