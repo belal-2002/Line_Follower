fbccd2df5eb5a6679fe7b3ed696a345ad1470888
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-29 00:31:14 +0300

    Create Buzzer.ino

---
+++ LineFollower/Buzzer.ino
@@ -0,0 +1,28 @@
+void loopBuzzer() {
+  switch (buzzer) {
+    case 0:
+
+    break;
+    case 1:
+ 
+    break;
+    case 2:
+
+    break;
+    case 3:
+
+    break;
+    case 4: 
+
+    break;
+    case 5: 
+
+    break;
+    case 6:
+
+    break;
+    case 7:
+
+    break;
+  }
+}
