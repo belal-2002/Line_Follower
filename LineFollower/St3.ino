8d6fa218cfd0f5795591403a092f5dd4da61a808
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-28 12:02:11 +0300

    Create St3.ino

---
+++ LineFollower/St3.ino
@@ -0,0 +1,60 @@
+void loopStrategy3() { //Right 
+  if (rightRadar && (midSensor >= 3) && (!leftRadar)) {
+    caseMotor = 2;
+    goRight = true; 
+    loopMotor();
+    delay(75);
+    turnStartTime = millis();
+    lineWasFound = false;
+    return;   
+  }
+
+  if (((bitRead(sensorBit, 5)) || (bitRead(sensorBit, 6))) && (!rightRadar)) { 
+    if (millis() - turnStartTime > 100) {
+    goLeft = false;
+    goRight = false;
+    }
+  }
+  
+  //if (goLeft && leftRadar) { goLeft = false; calculateError(); return; }
+  //if (goRight && rightRadar) { goRight = false; calculateError(); return; }
+  
+  if (goLeft || goRight) {
+  if (millis() - turnStartTime > 100) { // أقصى وقت مسموح للدوران الأعمى
+      goLeft = false; 
+      goRight = false;
+      caseMotor = 0; // العودة للوضع الطبيعي
+  }
+  return;
+}
+
+  if (!allSensor) {
+    lineWasFound = false;
+    if (rightRadarOn) {
+      caseMotor = 2;
+      goRight = true;
+      turnStartTime = millis();
+      return;
+    }
+    if (leftRadarOn) {
+      caseMotor = 1;
+      goLeft = true;
+      turnStartTime = millis();
+      return;
+    }
+
+  }
+
+  if (leftRadar && rightRadar && midSensor) {
+    bitClear(sensorBit, 0);
+    bitClear(sensorBit, 1);
+    bitClear(sensorBit, 10);
+    bitClear(sensorBit, 11);
+  }
+  else if (leftRadar && midSensor) {
+    bitClear(sensorBit, 10);
+    bitClear(sensorBit, 11);
+  }
+  
+  calculateError();
+}      
