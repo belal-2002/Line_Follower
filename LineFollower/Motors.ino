4ef48cb84005fa1f5107c8d05c635be9fe1c09a7
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-04-28 23:26:23 +0300

    Update Motors.ino

--- LineFollower/Motors.ino
+++ LineFollower/Motors.ino
@@ -2,8 +2,10 @@ void loopMotor() {
   switch (caseMotor) {
     case 0:
       // إرجاع المحركات للاتجاه الأمامي الطبيعي
-      digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
+      digitalWrite(AIN1, LOW);
+      digitalWrite(AIN2, HIGH); 
       digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW);
+      
       // تقييد السرعة وإرسالها للمحركات (10-bit)
       leftMotorSpeed  = constrain(baseSpeed + PD_Value, 0, maximumSpeed);
       rightMotorSpeed = constrain(baseSpeed - PD_Value, 0, maximumSpeed);
@@ -12,40 +14,53 @@ void loopMotor() {
       ledcWrite(PWMB, rightMotorSpeed);
     break;
     case 1:
-      // الدوران لليسار
-      digitalWrite(AIN1, HIGH); digitalWrite(AIN2, LOW); 
+      // الدوران لليسار (Left Turn)
+      // 1. المحرك الأيسر (الداخلي) يعمل كمرساة ويدور للخلف بسرعة منخفضة
+      digitalWrite(AIN1, HIGH); digitalWrite(AIN2, LOW);
+      ledcWrite(PWMA, innerTurnSpeed); // <-- تم التعديل للسرعة التفاضلية
+      
+      // 2. المحرك الأيمن (الخارجي) يستمر بالدفع للأمام بالسرعة العالية المحددة
       digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW); 
-    
-      ledcWrite(PWMA, turnSpeed);
-      ledcWrite(PWMB, turnSpeed);
+      ledcWrite(PWMB, turnSpeed);      // 600
     break;
     case 2:
-      // الدوران لليمين
+      // الدوران لليمين (Right Turn)
+      // 1. المحرك الأيسر (الخارجي) يستمر بالدفع للأمام بالسرعة العالية المحددة
       digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
+      ledcWrite(PWMA, turnSpeed);      // 600
+      
+      // 2. المحرك الأيمن (الداخلي) يعمل كمرساة ويدور للخلف بسرعة منخفضة
       digitalWrite(BIN1, LOW); digitalWrite(BIN2, HIGH); 
-    
-      ledcWrite(PWMA, turnSpeed);
-      ledcWrite(PWMB, turnSpeed);
+      ledcWrite(PWMB, innerTurnSpeed); // <-- تم التعديل للسرعة التفاضلية
     break;
     case 3:
       // السير في خط مستقيم
-      digitalWrite(AIN1, LOW); digitalWrite(AIN2, HIGH); 
+      digitalWrite(AIN1, LOW);
+      digitalWrite(AIN2, HIGH); 
       digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW);
 
       ledcWrite(PWMA, turnSpeed);
       ledcWrite(PWMB, turnSpeed);
     break;
     case 4:
-
+        digitalWrite(STBY, LOW);  
+        digitalWrite(AIN1, LOW);  digitalWrite(AIN2, LOW); 
+        digitalWrite(BIN1, LOW);  digitalWrite(BIN2, LOW);  
+        ledcWrite(PWMA, 0);
+        ledcWrite(PWMB, 0);
     break;
     case 5:
+      // السير في خط مستقيم
+      digitalWrite(AIN1, LOW);
+      digitalWrite(AIN2, HIGH); 
+      digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW);
 
+      ledcWrite(PWMA, turnSpeed);
+      ledcWrite(PWMB, turnSpeed);
     break;
     case 6:
-
     break;
     case 7:
-
     break;
   }
 }
