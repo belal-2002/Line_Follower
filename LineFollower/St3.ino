345d08897d847539505494a19e760054d038bc6f
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-05-01 16:13:43 +0300

    Update St3.ino

--- LineFollower/St3.ino
+++ LineFollower/St3.ino
@@ -1,49 +1,52 @@
-void loopStrategy3() { //Right 
-  if ((rightRadar == 2) && (midSensor >= 3) && (!leftRadar)) {
-    goRight = true; 
-    rightMotor();
+void loopStrategy3() { //Left
+  if ((leftRadar == 2) && (midSensor >= 3) && (!rightRadar)) {
+    goLeft = true; 
+    leftMotor();
     delay(75);
     turnStartTime = millis();
     lineWasFound = false;
     return;   
   }
 
-  if (((bitRead(sensorBit, 5)) || (bitRead(sensorBit, 6))) && (!rightRadar)) { 
+  if (((bitRead(sensorBit, 5)) || (bitRead(sensorBit, 6))) && (!leftRadar)) {
     //if (millis() - turnStartTime > 100) {
     goLeft = false;
     goRight = false;
     //}
   }
-  
+
   //if (goLeft && leftRadar) { goLeft = false; calculateError(); return; }
   //if (goRight && rightRadar) { goRight = false; calculateError(); return; }
-  
+
   if (goLeft || goRight) return;
 
   if (!allSensor) {
     lineWasFound = false;
-    if (rightRadarOn) {
-      goRight = true;
-      rightMotor();
-      return;
-    }
     if (leftRadarOn) {
       goLeft = true; 
       leftMotor();
       return;
     }
+    if (rightRadarOn) {
+      goRight = true;
+      rightMotor();      
+      return;
+    }
     return;
   }
 
+  // 1. اكتشاف تقاطع الزائد (+) وتجاوزه مستقيماً
   if (leftRadar && rightRadar && midSensor) {
+    // إعماء الرادارين معاً ليمر الروبوت بناءً على حساسات المنتصف فقط
     bitClear(sensorBit, 0);
     bitClear(sensorBit, 1);
     bitClear(sensorBit, 10);
     bitClear(sensorBit, 11);
   }
-  else if (leftRadar && midSensor) {
-    bitClear(sensorBit, 10);
-    bitClear(sensorBit, 11);
+  // 2. تجاهل الفخاخ اليمنى (حرف T المتجه لليمين)
+  else if (rightRadar && midSensor) {
+    bitClear(sensorBit, 0);
+    bitClear(sensorBit, 1);
   }
   
   calculateError();
