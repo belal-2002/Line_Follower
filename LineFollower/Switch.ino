80e4b3601987a81de882d2738a251e1405b094d5
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-05-01 03:48:05 +0300

    Update Switch.ino

--- LineFollower/Switch.ino
+++ LineFollower/Switch.ino
@@ -18,11 +18,7 @@ void loopSwitch() {
       if (isRunning) {
         digitalWrite(STBY, HIGH);
       } else {
-        digitalWrite(STBY, LOW);  
-        digitalWrite(AIN1, LOW);  digitalWrite(AIN2, LOW); 
-        digitalWrite(BIN1, LOW);  digitalWrite(BIN2, LOW);  
-        ledcWrite(PWMA, 0);
-        ledcWrite(PWMB, 0);
+        stopMotor();  
       }
       
       lastButtonPress = millis();
