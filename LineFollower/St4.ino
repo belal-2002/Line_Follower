e20ca0e4f9899ebd91774d4c133a6b2107e20266
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-05-02 04:24:02 +0300

    Update St4.ino

--- LineFollower/St4.ino
+++ LineFollower/St4.ino
@@ -5,14 +5,16 @@ void loopStrategy4() { //Right
     goRight = false;
   }
   
-  // الاستشفاء المبكر (إنهاء الدوران فور التقاط الرادار للخط)
+  // الاستشفاء المبكر
   if (goRight && rightRadar) { goRight = false; calculateError(); return; }
   if (goLeft && leftRadar) { goLeft = false; calculateError(); return; }
 
-  if (((bitRead(sensorBit, 5)) || (bitRead(sensorBit, 6))) && (!rightRadar)) { 
-    //if (millis() - turnStartTime > 100) {
-    turnRight = false;
-    //}
+  // --- التعديل الجديد باستخدام MPU6050 بدلاً من الوقت ---
+  if (turnRight) {
+    if ((abs(currentAngleZ) >= 70.0) || 
+        (abs(currentAngleZ) >= 50.0 && (bitRead(sensorBit, 5) || bitRead(sensorBit, 6)))) {
+      turnRight = false;
+    }
   }
   
   // الاستمرار في الدوران إذا بدأناه
@@ -35,15 +37,22 @@ void loopStrategy4() { //Right
     return;
   }
 
-  if ((rightRadar == 2) && (midSensor >= 3) && (!leftRadar)) {
+  // --- نقطة تفعيل الدوران ---
+  if ((rightRadar) && (midSensor >= 2) && (!leftRadar)) {
     turnRight = true; 
+    //goRight = true;
     rightMotor();
-    delay(85);
-    turnStartTime = millis();
+    
+    // تصفير الزاوية لحساب الـ 60 درجة بشكل دقيق
+    currentAngleZ = 0.0; 
+    // تمت إزالة delay(85) والاعتماد على الوقت هنا
+    
     lineWasFound = false;
     return;   
   }
 
+
+  /*
   if (leftRadar && rightRadar && midSensor) {
     bitClear(sensorBit, 0);
     bitClear(sensorBit, 1);
@@ -54,6 +63,6 @@ void loopStrategy4() { //Right
     bitClear(sensorBit, 10);
     bitClear(sensorBit, 11);
   }
-  
+  */
   calculateError();
-}      
+}
