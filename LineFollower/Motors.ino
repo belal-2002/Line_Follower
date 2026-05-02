722f20ba111af583dd71c9d128bbf97d631507ab
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-05-02 04:24:24 +0300

    Update Motors.ino

--- LineFollower/Motors.ino
+++ LineFollower/Motors.ino
@@ -47,8 +47,8 @@ void forwardMotor() {
   digitalWrite(AIN1, LOW);  digitalWrite(AIN2, HIGH); 
   digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW);
 
-  ledcWrite(PWMA, turnSpeed);
-  ledcWrite(PWMB, turnSpeed);
+  ledcWrite(PWMA, baseSpeed);
+  ledcWrite(PWMB, baseSpeed);
 }
 
 
