0bc355ef291779e8723070725f2657823d20163d
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-06-11 18:54:24 +0300

    Update Print.ino

--- LineFollower/Print.ino
+++ LineFollower/Print.ino
@@ -40,8 +40,8 @@ void loopPrint() {
     if (c == 'd') maximumSpeed -= 25;
     if (c == 'r') baseSpeed += 25;
     if (c == 'f') baseSpeed -= 25;
-    if (c == 't') {turnSpeed += 25; innerTurnSpeed = turnSpeed / 2;}
-    if (c == 'g') {turnSpeed -= 25; innerTurnSpeed = turnSpeed / 2;}
+    if (c == 't') {turnSpeed += 25; innerTurnSpeed = turnSpeed / 5*4;}
+    if (c == 'g') {turnSpeed -= 25; innerTurnSpeed = turnSpeed / 5*4;}
     if (c == '0') strategy = 0;
     if (c == '1') strategy = 1;
     if (c == '2') strategy = 2;
