d06803fc39c5a17fac00208980772a70163758a6
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-06-11 19:32:55 +0300

    Update PD.ino

--- LineFollower/PD.ino
+++ LineFollower/PD.ino
@@ -1,16 +1,9 @@
 void calculatePD() {
-  P = 0;
-  D = 0;
-  currentTime = micros();
-
-  dt = (float) (currentTime - lastTime) / 1000000.0;
-  lastTime = currentTime;
-
-  if (dt <= 0.0) dt = 0.001;
-
   P = currentError;
-  D = (currentError - lastError) / dt;
-  lastError = currentError; 
+  // حذف القسمة على dt تماماً لمنع الفرملة العكسية العنيفة
+  D = currentError - lastError; 
+  lastError = currentError;
+  
   PD_Value = (Kp * P) + (Kd * D);
 
   moveMotor();
