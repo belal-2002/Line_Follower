791108ac45d51147ec9b267f4b8ccf50a54de745
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-06-18 07:05:45 +0300

    Update MPU.ino

--- LineFollower/MPU.ino
+++ LineFollower/MPU.ino
@@ -1,19 +1,63 @@
 void updateMPU() {
+  // قراءة سجلات مقياس التسارع (من سجل 0x3B إلى 0x40 لـ X, Y, Z)
   Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x47);
+  Wire.write(0x3B); 
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_ADDR, 2, true);
-  int16_t gyroZ_raw = Wire.read() << 8 | Wire.read();
+  Wire.requestFrom(MPU_ADDR, 6, true);
+  
+  int16_t ax = Wire.read() << 8 | Wire.read();
+  int16_t ay = Wire.read() << 8 | Wire.read();
+  int16_t az = Wire.read() << 8 | Wire.read();
 
-  unsigned long currentTime = micros();
-  float dt = (currentTime - lastMpuTime) / 1000000.0;
-  lastMpuTime = currentTime;
+  // حساب زاوية الميلان الرأسي (Pitch) بالدرجات
+  float rawPitch = atan2((float)-ax, sqrt((float)ay * ay + (float)az * az)) * 180.0 / PI;
+  pitchAngle = rawPitch - pitchOffset;
 
-  // تحويل القراءة الخام إلى درجات/ثانية
-  float gyroZ_rate = (gyroZ_raw - gyroZ_offset) / 131.0;
-
-  // فلتر بسيط لتجاهل التشويش (Deadband)
-  if(abs(gyroZ_rate) > 1.0) {
-    currentAngleZ += gyroZ_rate * dt;
+  // --- منطق التحكم بالسرعة الذكي حسب حالة المنحدر ---
+  
+  if (pitchAngle > 10.0) { 
+    // حالة الصعود: الروبوت مائل للأعلى -> زيادة السرعة فوراً للمساعدة في التغلب على الجاذبية
+    maximumSpeed = originalMaximumSpeed * 2;
+    baseSpeed = originalBaseSpeed * 2;
+    turnSpeed = originalTurnSpeed * 2;
+  } 
+  else if (pitchAngle < -10.0) {
+    // حالة النزول: الروبوت مائل للأسفل -> تقليل السرعة لمنع الانزلاق العنيف أو فقدان الخط
+    maximumSpeed = originalMaximumSpeed / 2;
+    baseSpeed = originalBaseSpeed / 2;
+    turnSpeed = originalTurnSpeed / 2;
+  } 
+  else {
+    // الأرض المستوية: سرعة طبيعية بدون أي تعديل
+    maximumSpeed = originalMaximumSpeed;
+    baseSpeed = originalBaseSpeed;
+    turnSpeed = originalTurnSpeed;
   }
 }
+
+void resetPitchOffset() {
+  float sumPitch = 0;
+  // نأخذ متوسط 10 قراءات سريعة جداً لضمان دقة نقطة الصفر (المدة الكلية 20 ملي ثانية)
+  for (int i = 0; i < 10; i++) {
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(0x3B); 
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_ADDR, 6, true);
+    
+    int16_t ax = Wire.read() << 8 | Wire.read();
+    int16_t ay = Wire.read() << 8 | Wire.read();
+    int16_t az = Wire.read() << 8 | Wire.read();
+
+    // حساب الزاوية الخام اللحظية بنفس الطريقة (استخدمنا الطريقة الأولى التي اتفقنا عليها)
+    float rawPitch = atan2((float)-ax, sqrt((float)ay * ay + (float)az * az)) * 180.0 / PI;
+    sumPitch += rawPitch;
+    delay(2);
+  }
+  
+  // حفظ المتوسط كنقطة مرجعية
+  pitchOffset = sumPitch / 10.0; 
+}
+
+
+
+
