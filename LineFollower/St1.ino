a270d4801753c91e81906355644f1426ef0ff4c9
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-06-24 17:52:55 +0300

    Update St1.ino

--- LineFollower/St1.ino
+++ LineFollower/St1.ino
@@ -1,9 +1,4 @@
-void loopStrategy1() { //Left
-
-   if(midSensor){
-    lostLineDistance = totalOdometer;
-   }
-
+void loopStrategy1() { // للأمام ثم لليسار
   // إلغاء الدوران الأعمى فور ملامسة حساسات المنتصف للخط
   if (midMidSensor) { 
     if (goLeft || goRight){
@@ -16,20 +11,9 @@ void loopStrategy1() { //Left
     goRight = false;
   }
 
-
-
-  // الاستشفاء المبكر
+  // الاستشفاء المبكر (إنهاء الدوران فور التقاط الرادار للخط)
   if (goLeft && leftMidRadar) { goLeft = false; calculateError(); return; }
   if (goRight && rightMidRadar) { goRight = false; calculateError(); return; }
-
-  // --- التعديل الجديد باستخدام MPU6050 بدلاً من الوقت ---
-  if (turnLeft) {
-    if ((abs(currentAngleZ) >= 70.0) || 
-        (abs(currentAngleZ) >= 50.0 && (midMidSensor)) ||
-        (millis() - turnStartTime >= 350)) {  // <-- إضافة شرط الـ 400 ملي ثانية هنا
-      turnLeft = false;
-    }
-  }
   
   // الاستمرار في الدوران إذا بدأناه
   if (turnLeft || goLeft || goRight) return;
@@ -47,54 +31,10 @@ void loopStrategy1() { //Left
       rightMotor();      
       return;
     }
-    LineNotFoundTime = millis();
-    if ((totalOdometer - lostLineDistance) > gapDistance && !allSensor) {
-      if (!Turn180now) {  // الاستشفاء عبر الدوران الموضعي 180 درجة (الملاذ الأخير)
-        Turn180now = true;  // إذا لم نكن في حالة الدوران، نبدأها الآن
-        sweep180Done = false;
-        resetAngleZ(); // تصفير الزاوية لبدء حساب 180 درجة
-        sweepSearchTurn(); // بدء الدوران 
-        return;
-      } else {  
-        if (sweep180Done) {  // نحن الآن في منتصف عملية الدوران، نتحقق من الزاوية
-        //if (abs(currentAngleZ) >= 180.0) {  // نحن الآن في منتصف عملية الدوران، نتحقق من الزاوية
-            // اكتملت الـ 180 درجة ولم يجد الخط
-            forwardMotor(); // الاتجاه للأمام كما طلبت
-            return;
-        } else {
-            // لم يكمل 180 درجة بعد، استمر بالدوران حول نفسه
-            sweepSearchTurn();
-            return;
-          }
-        }
-    } else { 
-      forwardMotor();
-      return;  
-    }
+    forwardMotor();
+    return;
   }
-  if (millis() - LineNotFoundTime >= 10) {
-    Turn180now = false; // إنهاء حالة البحث
-    sweep180Done = false;
-    lostLineDistance = totalOdometer;
-  }
-
-  // --- نقطة تفعيل الدوران ---
-  if ((leftMidRadar) && (midMidSensor >= 3) && (!rightMidRadar)) {
-    turnLeft = true; 
-    //goLeft = true;
-    leftMotor();
-    // تصفير الزاوية ليبدأ الحساب الدقيق من لحظة اتخاذ قرار الدوران
-    resetAngleZ(); 
-    // تمت إزالة delay(85) والاعتماد على الوقت هنا
-    turnStartTime = millis();
-    lineWasFound = false;
-    return;   
-  }
-
-  bitClear(sensorBit, 0);
-  bitClear(sensorBit, 9);
 
 
   calculateError();
-  
 }
