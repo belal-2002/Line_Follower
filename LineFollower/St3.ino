1df423218142a14a4e7eabd0adf98f13f64bb1af
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-06-26 17:16:51 +0300

    Update St3.ino

--- LineFollower/St3.ino
+++ LineFollower/St3.ino
@@ -1,16 +1,18 @@
 void loopStrategy3() { // Left Strategy
+
   cancelBlindTurn_1();
   
-  if (checkEarlyRecovery_2(leftMidRadar, rightMidRadar)) return;
+  if (checkEarlyRecovery_2()) return;
   
   checkMPUTurnCompletion_3(70.0, 50.0, 350); // دوران بزاوية 70 وتايم أوت 350ms
   
   if (isCurrentlyTurning_4()) return;
   
-  if (handleLineLoss_5(midSensor, leftMidRadarOn, rightMidRadarOn)) return;
+  if (handleLineLoss_5()) return;
 
-  if (activateTurn_6(true, leftMidRadar, 2, rightRadar)) return; // true تعني دوران يسار
+  if (activateTurn_6()) return; 
 
   ignoreIntersections_7();
+
   calculateError();
 }
