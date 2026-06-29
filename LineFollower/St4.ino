1342bef62c70f23ca7f6f13b169bdb8e3c1eec8f
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-06-29 07:13:23 +0300

    Update St4.ino

--- LineFollower/St4.ino
+++ LineFollower/St4.ino
@@ -1,12 +1,14 @@
-void loopStrategy4() { 
+void loopStrategy4() {  //يسار
 
   cancelBlindTurn_1();
   
-  if (checkEarlyRecovery_2()) return;
+  checkMPUTurnCompletion_3();
 
   if (isCurrentlyTurning_4()) return;
 
   if (handleLineLoss_5()) return;
+
+  if (activateTurn_6()) return; 
 
   ignoreIntersections_7();
 
