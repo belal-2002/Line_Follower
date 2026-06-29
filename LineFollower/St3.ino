be935b6d30d65a2c5f9df974f5057f5c8cef670f
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-06-29 07:13:25 +0300

    Update St3.ino

--- LineFollower/St3.ino
+++ LineFollower/St3.ino
@@ -1,16 +1,12 @@
-void loopStrategy3() { // Left Strategy
+void loopStrategy3() {  //لامام ثم يسار
 
   cancelBlindTurn_1();
   
-  if (checkEarlyRecovery_2()) return;
-  
-  checkMPUTurnCompletion_3(70.0, 50.0, 350); // دوران بزاوية 70 وتايم أوت 350ms
-  
-  if (isCurrentlyTurning_4()) return;
-  
-  if (handleLineLoss_5()) return;
+  //if (checkEarlyRecovery_2()) return;
 
-  if (activateTurn_6()) return; 
+  if (isCurrentlyTurning_4()) return;
+
+  if (handleLineLoss_5()) return;
 
   ignoreIntersections_7();
 
