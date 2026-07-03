0495175e353a66920a6140e8def6bd85dfe184c0
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-07-03 07:22:52 +0300

    Update St1.ino

--- LineFollower/St1.ino
+++ LineFollower/St1.ino
@@ -1,40 +1,12 @@
 void loopStrategy1() { // للأمام ثم لليسار
-  // إلغاء الدوران الأعمى فور ملامسة حساسات المنتصف للخط
-  if (midMidSensor) { 
-    if (goLeft || goRight){
-      leftRadarOn = false;
-      rightRadarOn = false;
-      leftMidRadarOn = false;
-      rightMidRadarOn = false;
-    }
-    goLeft = false;
-    goRight = false;
-  }
 
-  // الاستشفاء المبكر (إنهاء الدوران فور التقاط الرادار للخط)
-  if (goLeft && leftMidRadar) { goLeft = false; calculateError(); return; }
-  if (goRight && rightMidRadar) { goRight = false; calculateError(); return; }
-  
-  // الاستمرار في الدوران إذا بدأناه
-  if (turnLeft || goLeft || goRight) return;
+  cancelBlindTurn_1();
 
-  // الدخول في حالة الفقدان الكلي للخط
-  if (!midSensor) {
-    lineWasFound = false;
-    if (leftMidRadarOn) {
-      goLeft = true; 
-      leftMotor();
-      return;
-    }
-    if (rightMidRadarOn) {
-      goRight = true;
-      rightMotor();      
-      return;
-    }
-    forwardMotor();
-    return;
-  }
+  if (isCurrentlyTurning_4()) return;
 
+  if (handleLineLoss_5()) return;
+
+  ignoreIntersections_7();
 
   calculateError();
 }
