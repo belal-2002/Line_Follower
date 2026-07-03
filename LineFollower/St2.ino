f8d5fd9241486868157548dccebe9e8872a58567
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-07-03 10:07:23 +0300

    Update St2.ino

--- LineFollower/St2.ino
+++ LineFollower/St2.ino
@@ -1,19 +1,4 @@
 void loopStrategy2() { // Left Strategy
-
-  cancelBlindTurn_1();
-  
-  //if (checkEarlyRecovery_2()) return;
-  
-  //checkMPUTurnCompletion_3();
-  
-  if (isCurrentlyTurning_4()) return;
-  
-  if (handleLineLoss_5()) return;
-
-  if (activateTurn_6()) return; 
-
-  ignoreIntersections_7();
-
-  calculateError();
+calculateError();
 }
   
