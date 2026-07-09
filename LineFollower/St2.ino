b053bcd390688ab0f279f141729a5fd24b42f2df
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-07-09 07:26:51 +0300

    Update St2.ino

--- LineFollower/St2.ino
+++ LineFollower/St2.ino
@@ -1,4 +1,16 @@
-void loopStrategy2() {
+void loopStrategy2() { // للأمام ثم لليسار
+
+  cancelTurn_1();
+
+  if (isTurning_2()) return;
+
+  if (activateGo_3()) return;
+
+  if (noLine_4()) return;
+
+  cleanIR_6();
+
   calculateError();
 }
+
   
