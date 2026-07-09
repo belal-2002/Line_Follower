338f04c0e4c73417288ccca304c12bfc297ac236
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-07-09 12:42:50 +0300

    Update St3.ino

--- LineFollower/St3.ino
+++ LineFollower/St3.ino
@@ -6,11 +6,11 @@ void loopStrategy3() { // لليسار
 
   if (activateGo_3()) return;
 
-  if (noLine_4()) return;
+  if (gap_4()) return;
 
-  if (activateTurn_5(1)) return; 
+  if (activateTurn_5()) return; 
 
-  cleanIR_6();
+  //cleanIR_6();
 
   calculateError();
 }
