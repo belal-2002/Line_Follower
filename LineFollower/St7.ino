be512d1511bfdc851f33029bf5749daea0871809
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-07-10 05:32:26 +0300

    Update St7.ino

--- LineFollower/St7.ino
+++ LineFollower/St7.ino
@@ -1,3 +1,18 @@
 void loopStrategy7() {
-  forwardMotor();
+    if (midSensor >= 5){
+    leftMotor();
+    return;
+  }
+
+  cancelTurn_1();
+
+  if (isTurning_2()) return;
+
+  if (activateGo_3()) return;
+
+  if (gap_4()) return;
+
+  cleanIR_6();
+
+  calculateError();
 }
