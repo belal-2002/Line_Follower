c713f9df7401942f3569ae3d12b350d3d4d4e1b2
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-07-10 05:32:28 +0300

    Update St6.ino

--- LineFollower/St6.ino
+++ LineFollower/St6.ino
@@ -1,3 +1,20 @@
 void loopStrategy6() {
+  if (midSensor >= 4){
+    leftMotor();
+    return;
+  }
+  
+  cancelTurn_1();
+
+  if (isTurning_2()) return;
+
+  if (activateGo_3()) return;
+
+  if (gap_4()) return;
+
+  if (activateTurn_5()) return; 
+
+  cleanIR_6();
+
   calculateError();
 }
