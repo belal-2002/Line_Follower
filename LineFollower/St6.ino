0235a462abe472b9583a18ec651c8a7d3f455c35
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-07-11 14:23:09 +0300

    Update St6.ino

--- LineFollower/St6.ino
+++ LineFollower/St6.ino
@@ -1,22 +1,26 @@
 void loopStrategy6() {
-  enableInversionDetection = true;
-  
-  if (midSensor >= 4){
-    leftMotor();
-    return;
+if (isInverted) { 
+    isPostInversion = true; 
   }
-  
-  cancelTurn_1();
 
-  if (isTurning_2()) return;
-
-  if (activateGo_3()) return;
-
-  if (gap_4()) return;
-
-  if (activateTurn_5()) return; 
-
-  cleanIR_6();
-
-  calculateError();
+  if (!isPostInversion) {
+    enableInversionDetection = true;
+    if (midSensor >= 6){
+      leftMotor();
+      return;
+    }
+    cancelTurn_1();
+    if (isTurning_2()) return;
+    if (activateGo_3()) return; 
+    if (gap_4()) return;
+    cleanIR_6();
+    calculateError();
+  } else {
+    cancelTurn_1();
+    if (isTurning_2()) return;
+    if (activateGo_3()) return;
+    if (gap_4()) return;
+    cleanIR_6();
+    calculateError();
+  }
 }
