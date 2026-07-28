4f76bed4352a4ef6f8f33fb4f552a33b2a87093c
belal-2002 <159795094+belal-2002@users.noreply.github.com>
2026-07-28 10:08:56 +0300

    Update St7.ino

--- LineFollower/St7.ino
+++ LineFollower/St7.ino
@@ -1,23 +1,3 @@
 void loopStrategy7() {
-if (isInverted) { 
-    isPostInversion = true; 
-  }
-
-  if (!isPostInversion) {
-    enableInversionDetection = true;
-    cancelTurn_1();
-    if (isTurning_2()) return;
-    if (activateGo_3(true)) return; 
-    if (gap_4()) return;
-    if (activateTurn_5()) return; 
-    cleanIR_6();
-    calculateError();
-  } else {
-    cancelTurn_1();
-    if (isTurning_2()) return;
-    if (activateGo_3()) return;
-    if (gap_4()) return;
-    cleanIR_6();
-    calculateError();
-  }
+  forwardMotor();
 }
